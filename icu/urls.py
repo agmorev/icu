3d6b45a98103b14d01e4965248e4cb2bc4aa6979
agmorev <agmorev@gmail.com>
2020-03-20 01:42:33 +0200

    Installing main template

---
+++ icu/urls.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.flatpages import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.flatpage, {'url': '/home/'}, name='home'),
+    path('contacts/', views.flatpage, {'url': '/contacts/'}, name='contacts'),
+    path('accounts/', include('allauth.urls')),
+]
